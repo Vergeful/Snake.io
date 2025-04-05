36cc4d3d863e12cf379076cb8c94ec930d5d8f77
Arne-Hilao <arne.hilao@gmail.com>
2025-04-04 21:34:52 -0600

    Fixed replica5

--- backend/replica5_project/replica5_app/apps.py
+++ backend/replica5_project/replica5_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Replica5AppConfig(AppConfig):
+class replica5AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'replica5_app'
--- backend/replica5_project/replica5_project/wsgi.py
+++ backend/replica5_project/replica5_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for replica4_project project.
+WSGI config for replica5_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'replica4_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'replica5_project.settings')
 
 application = get_wsgi_application()
