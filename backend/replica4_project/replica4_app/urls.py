c7b8c402cc9f71eeadbc01dad18af6e768148fbf
Arne Hilao <arne.hilao@gmail.com>
2025-04-12 14:16:27 -0600

    updated url for the food list

--- backend/replica4_project/replica4_app/urls.py
+++ backend/replica4_project/replica4_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("create_player/", views.create_player, name= "create_player"),
     path("health_check/", views.health_check, name= "health_check"),
-    path("update_primary/", views.update_primary, name= "update_primary")
+    path("update_primary/", views.update_primary, name= "update_primary"),
+    path("update_food_list/", views.update_local_food_list, name= "update_local_food_list")
 ]
