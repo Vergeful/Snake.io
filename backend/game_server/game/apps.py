93f71118ff72952a0d652d0664cc89ce729ac53f
riannt <rian.tapang@ucalgary.ca>
2025-04-14 05:27:32 -0600

    merging

--- backend/game_server/game/apps.py
+++ backend/game_server/game/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProxyAppConfig(AppConfig):
+class GameConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'proxy_app'
+    name = 'game'
